f21c5f9c68d9a470d231de470f69ee6940efddae
Chialiang86 <a930039930039@gmail.com>
2022-01-13 00:46:25 +0800

    add Readme.md and clean some files

--- SwinIR/run.sh
+++ SwinIR/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ $1 = "train" ]; then
     echo task : $1
-    config='options/swinir/train_swinir_sr_classical.json'
+    config='config/train_swinir_sr_classical.json'
     python3 main_train_psnr.py --opt $config
 
 elif [ $1 = "test" ]; then
     echo task : $1
     runnning_id=$2
     model_root='superresolution/swinir_sr_classical_patch48_x3_crop_1/models'
-    model_ids=("205000_G" "210000_G")
+    model_ids=("100000_G" "105000_G" "110000_G" "115000_G" "120000_G" "125000_G" "130000_G" "135000_G" "140000_G" "145000_G" "150000_G")
     inference="inference_${2}"
     imsize=48
     crop='no'
@@ -21,7 +21,6 @@ elif [ $1 = "test" ]; then
             --inf_root $inference \
             --model_path "${model_root}/${model_id}.pth" \
             --folder_lq 'dataset/testing_lr_images'
-        echo "zip ${inference}/${model_id}.zip ${inference}/${model_id}_${imsize}_3/*.png"
 
         if [ $crop = "yes" ]; then
             python3 puzzle.py -s "${inference}/${model_id}_${imsize}_3"
@@ -29,6 +28,18 @@ elif [ $1 = "test" ]; then
 
     done
 
+elif [ $1 = "best" ]; then
+    python3 main_test_swinir.py --task classical_sr \
+            --scale 3 \
+            --training_patch_size 48 \
+            --inf_root best \
+            --model_path "100000_G.pth" \
+            --folder_lq 'dataset/testing_lr_images'
+
+elif [ $1 = "crop" ]; then
+    echo "task : $1"
+    echo "img size : $2"
+    python3 training_patch.py -r dataset -s training_hr_images -hr $2 
 else 
     echo "error argument : " $1
 fi
---
+++ SwinIR/training_patch.py
@@ -0,0 +1,53 @@
+import cv2 
+import os
+from tqdm import tqdm
+import argparse
+
+def main(args):
+
+    src_path = f'{args.root}/{args.src}/'
+    dst_hr_path = f'{args.root}/hr_{args.hr}/'
+    
+    if not os.path.exists(src_path):
+        raise Exception(f'src dir : {src_path} not exists')
+    if not os.path.exists(dst_hr_path):
+        os.mkdir(dst_hr_path)
+        print(f'{dst_hr_path} created.')
+
+    img_files = os.listdir(src_path)
+    for img_file in tqdm(img_files):
+        hr_img = cv2.imread(f'{src_path}/{img_file}')
+        size = hr_img.shape[:2]
+
+        # high resolution
+        hh_num = size[0] // args.hr + 1
+        hw_num = size[1] // args.hr + 1
+        lid = 0
+        for hi in range(hh_num):
+            for wi in range(hw_num):
+                # case 1 
+                if hi == hh_num - 1 and wi == hw_num - 1:
+                    hr_crop = hr_img[-args.hr:, -args.hr:]
+                # case 2 
+                elif hi == hh_num - 1:
+                    hr_crop = hr_img[-args.hr:, (wi * args.hr): (wi * args.hr + args.hr)]
+                # case 3 
+                elif wi == hw_num - 1:
+                    hr_crop = hr_img[(hi * args.hr): (hi * args.hr + args.hr), -args.hr:]
+                # case 4 
+                else :
+                    hr_crop = hr_img[(hi * args.hr): (hi * args.hr + args.hr), (wi * args.hr): (wi * args.hr + args.hr)]
+                
+                if hr_crop.shape[:2] != (args.hr, args.hr):
+                    hr_crop = cv2.resize(hr_crop, (args.hr, args.hr), interpolation=cv2.INTER_CUBIC)
+                cv2.imwrite(f'{dst_hr_path}/{img_file[:-4]}_{size[0]}_{size[1]}_{lid}.png', hr_crop)
+                lid += 1
+            
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', '-r', default='dataset', type=str)
+    parser.add_argument('--src', '-s', default='training_hr_images', type=str)
+    parser.add_argument('--hr', '-hr', default=96, type=int)
+    args = parser.parse_args()
+    
+    main(args)
